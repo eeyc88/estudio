409b4ff4ad7e297393739790d257b51096c44f4c
eeyc88 <erick-yepez-cedeno@hotmail.com>
2018-11-26 14:56:32 -0500

    archivos movidos

---
+++ tabla.php
@@ -0,0 +1,40 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>tabla de multiplicar</title>
+</head>
+<body>
+<header>
+	<link rel="stylesheet" href="style.css" type="text/css">
+</header>
+
+<?php
+	echo "hola <br>";
+	for($x=1;$x<=10;$x++){
+		echo '<section id="operacion">';
+		for($y=1;$y<=10;$y++){
+			echo $x." * ".$y." = ".($x*$y).'<br>';
+		}
+		echo '</section>';
+	}
+?>
+
+<section>
+	<article>
+		<aside>
+			<form action="datos.php" method="get">
+				Nombre:<input type="text" name="firstname" value=""><br>
+				Apellido:<input type="text" name="lastname" value=""><br>
+				<input type="submit" value="Enviar">
+			</form>
+		</aside>
+	</article>
+</section>
+<footer>
+</footer>
+</body>
+</html>
